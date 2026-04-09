1a7f606dfc7df6e89fe4cac1e83475b6498ddbfa
Shreya Kumari <108955769+skumari2023@users.noreply.github.com>
2026-04-09 14:27:41 -0500

    Add: duplicate-integer - submission-23

---
+++ Data Structures & Algorithms/duplicate-integer/submission-23.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        dup = set()
+        for n in nums:
+            if n in dup:
+                return True
+            dup.add(n)
+        return False
