15bb324663735e641c84883a15dbb78843dac5e7
Shreya Kumari <108955769+skumari2023@users.noreply.github.com>
2026-04-09 21:19:28 -0500

    Add: count-connected-components - submission-2

---
+++ Data Structures & Algorithms/count-connected-components/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        
+        count = 0
+        visited = set()
+        #adjacency list holds all vertices + edges = O(V+E) space complexity
+        graph = defaultdict(list)
+
+        def dfs(i):
+            visited.add(i)
+            for j in graph[i]:
+                if j not in visited:
+                    dfs(j)
+        
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count += 1
+        return count
