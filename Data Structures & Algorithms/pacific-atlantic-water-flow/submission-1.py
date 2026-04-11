a5f1546e9491b66b7e150bc2c2d737bde2d64cc9
Shreya Kumari <108955769+skumari2023@users.noreply.github.com>
2026-04-11 00:43:04 -0500

    Add: pacific-atlantic-water-flow - submission-1

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        rows = len(heights)
+        cols = len(heights[0])
+        pac = set()
+        atl = set()
+        output = []
+
+        def dfs(r,c,visited,prevHeight):
+            #traversing from ocean inwards --> working backwards that's why heights[r][c] < prevHeight
+            if r < 0 or c < 0 or r > rows - 1 or c > cols - 1 or (r,c) in visited or heights[r][c] < prevHeight:
+                return
+            
+            visited.add((r,c))
+
+            dfs(r+1,c,visited,heights[r][c])
+            dfs(r-1,c,visited,heights[r][c])
+            dfs(r,c+1,visited,heights[r][c])
+            dfs(r,c-1,visited,heights[r][c])
+
+        for c in range(cols):
+            dfs(0,c,pac,heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows-1][c])
+        
+        for r in range(rows):
+            dfs(r,0,pac,heights[r][0])
+            dfs(r,cols-1,atl,heights[r][cols-1])
+        
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    output.append([r,c])
+        return output
