d4bec9467a43b5d6a32926343ae6a08df5df5474
Shreya Kumari <108955769+skumari2023@users.noreply.github.com>
2026-09-11 00:09:21 -0500

    Add: is-anagram - submission-35

---
+++ Data Structures & Algorithms/is-anagram/submission-35.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+#space complexity dependent on the # of keys in hashmap = O(1) for this problem because there are only 26 letters in the alphabet
+
+        if len(s) != len(t):
+            return False 
+
+        h1 = {}
+        h2 = {}
+
+        for c in s:
+            if c in h1:
+                h1[c] += 1 #h1[c] = h1.get(c, 0) + 1
+            else:
+                h1[c] = 1
+        
+        for ch in t:
+            if ch in h2:
+                h2[ch] += 1
+            else:
+                h2[ch] = 1
+        
+        return h1 == h2
